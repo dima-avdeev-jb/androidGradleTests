5473ac539cab20ccd18cfd2ed8d67c19b26d2f7f
dima.avdeev <dima.avdeev@jetbrains.com>
2023-03-08 18:48:57 +0400

    copy

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,46 @@
-group "com.example"
-version "1.0-SNAPSHOT"
+plugins {
+    kotlin("multiplatform") apply false
+    id("com.android.library") apply false
+}
 
 allprojects {
     repositories {
         google()
         mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        mavenLocal()
     }
 }
 
-plugins {
-    kotlin("multiplatform") apply false
-    kotlin("android") apply false
-    id("com.android.application") apply false
-    id("com.android.library") apply false
+subprojects {
+    version = findProperty("deploy.version") ?: property("compose.version")!!
+
+    plugins.withId("java") {
+        configureIfExists<JavaPluginExtension> {
+            sourceCompatibility = JavaVersion.VERSION_11
+            targetCompatibility = JavaVersion.VERSION_11
+
+            withJavadocJar()
+            withSourcesJar()
+        }
+    }
+
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>() {
+        kotlinOptions.jvmTarget = "11"
+    }
+
+    plugins.withId("maven-publish") {
+        configureIfExists<PublishingExtension> {
+            repositories {
+                maven {
+                    name = "ComposeRepo"
+                    setUrl(System.getenv("COMPOSE_REPO_URL"))
+                    credentials {
+                        username = System.getenv("COMPOSE_REPO_USERNAME")
+                        password = System.getenv("COMPOSE_REPO_KEY")
+                    }
+                }
+            }
+        }
+    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,25 @@
 pluginManagement {
     repositories {
-        google()
         gradlePluginPortal()
         mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        google()
+        mavenLocal()
     }
 
     plugins {
+        kotlin("jvm").version(extra["kotlin.version"] as String)
         kotlin("multiplatform").version(extra["kotlin.version"] as String)
-        kotlin("android").version(extra["kotlin.version"] as String)
-        id("com.android.application").version(extra["agp.version"] as String)
+        id("org.jetbrains.compose").version(extra["compose.version"] as String)
         id("com.android.library").version(extra["agp.version"] as String)
     }
 }
 
-rootProject.name = "demo"
-
-include(":common")
+include(":SplitPane:library")
+include(":SplitPane:demo")
+include(":AnimatedImage:library")
+include("AnimatedImage:demo")
+include(":resources:library")
+include(":resources:demo:androidApp")
+include(":resources:demo:desktopApp")
+include(":resources:demo:shared")
